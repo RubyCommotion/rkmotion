4d612d733850f405ec32ac0ccbac4744a99bae24
Chris McKnight <cmckni3@gmail.com>
2014-04-11 22:21:06 -0500

    Fixes loading of cocoapods #1 Adds version 0.22 of RestKit using cocoapods

--- lib/rkmotion.rb
+++ lib/rkmotion.rb
@@ -1,3 +1,6 @@
+require 'motion-cocoapods'
+require 'motion-require'
+
 unless defined?(Motion::Project::Config)
   raise "This file must be required within a RubyMotion project Rakefile."
 end
@@ -6,4 +9,7 @@ lib_dir_path = File.dirname(File.expand_path(__FILE__))
 Motion::Project::App.setup do |app|
   gem_files = Dir.glob(File.join(lib_dir_path, 'rkmotion/**/*.rb'))
   app.files.unshift(gem_files).flatten!
+  app.pods do
+    pod 'RestKit', '~> 0.22.0'
+  end
 end
